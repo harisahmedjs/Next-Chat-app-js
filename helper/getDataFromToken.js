d486dfcb583f65c7204b576efe86262eb0fb3134
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-09-08 21:46:44 +0500

    getting updates

--- helper/getDataFromToken.js
+++ helper/getDataFromToken.js
@@ -6,6 +6,7 @@ export const getDataFromToken = (request) =>{
 try {
     const token =  request.cookies.get("token")?.value || ""
     const decodedToken = jwt.verify(token , process.env.SECRET_TOKEN)
+    return decodedToken.id
 } catch (error) {
     throw new Error(error.message)
 }
